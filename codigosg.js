3db4f1d6cffac9e221aa8290b34c4e1ba5dcff10
italoG27 <italogomez436@gmail.com>
2026-09-02 11:18:41 -0600

    inicio de sesion mejorado

--- codigosg.js
+++ codigosg.js
@@ -1,21 +1,43 @@
 var miUsuario = "";
 var miClave = "";
-
+//guaarda los datos y las contras del usuario en el reistro cchilero
 function guardarUsuario() {
     var txtUser = document.getElementById("campoUserReg").value;
     var txtPass = document.getElementById("campoPassReg").value;
 
-    if (txtUser == "admin" && txtPass == "1234") {
+
+    //primer usuario que esta permitido en el registro desde "ana123" hasta "lucia99" si es correcto te va a tirar un mensaje de aviso y si no es correcto no te va a dejar registrarte primo
+    if (txtUser == "ana123" && txtPass == "clave123") {
         miUsuario = txtUser;
         miClave = txtPass;
         alert("Te registrarte engazado Ahora puedes iniciar sesión primo .");
     } else {
-        alert("Para registrarte bien chilero, el usuario debe ser 'admin' y la contraseña '1234'");
+       
     }
-    
+
+
+    if (txtUser == "carlos_dev" && txtPass == "codigo2026") {
+        miUsuario = txtUser;
+        miClave = txtPass;
+        alert("Te registrarte engazado Ahora puedes iniciar sesión primo .");
+    } else {
+       
+    }
+
+    if (txtUser == "lucia99" && txtPass == "segura77") {
+        miUsuario = txtUser;
+        miClave = txtPass;
+        alert("Te registrarte engazado Ahora puedes iniciar sesión primo .");
+    } else {
+        
+    }
+
+
+
+
 
 }
-
+ //valida los datos que se van a ingresar en el login si es correcto te dejara pasar y si no es correcto te tira un mensaje de que los datos son incorrectos o que todavia no te registrarte con ese usuario
 function validarIngreso() {
     var userIngresado = document.getElementById("campoUserLog").value;
     var passIngresada = document.getElementById("campoPassLog").value;
@@ -26,4 +48,62 @@ function validarIngreso() {
     } else {
         alert("Datos incorrectos o aún no te has registrado ponete las pilas primo.");
     }
+}    
+
+// el arrays con los datos registrados
+const usuariosRegistrados = ["ana123", "carlos_dev", "lucia99"];
+const contraseniasRegistradas = ["clave123", "codigo2026", "segura77"];
+
+// datsos que va a ingresar el usuario a ver si esta bien o no 
+const usuarioIngresado = "carlos_dev";
+const contraseniaIngresada = "codigo2026";
+
+let accesoPermitido = false;
+
+// bucle de arrayts
+for (let i = 0; i < usuariosRegistrados.length; i++) {
+    // Comparamos el usuario y la contraseña en la misma posición 'i'
+    if (usuariosRegistrados[i] === usuarioIngresado && contraseniasRegistradas[i] === contraseniaIngresada) {
+        accesoPermitido = true;
+        break; // se para el bucle porque se encontro el dato correcto primo
+    }
 }
+
+// mensaje de aviso si es correcto o no le va a tirar distintos mensajes de aviso
+if (accesoPermitido) {
+    console.log("¡Acceso correcto! Bienvenido, " + usuarioIngresado);
+} else {
+    console.log("Usuario o contraseña incorrectos.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
